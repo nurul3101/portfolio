bfe7f989d47b1ab60670a7d907e062355ad822fa
Nurul Sundarani <sundarani@prisma.io>
2022-10-29 17:14:50 +0530

    Sanity Config

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,8 +4,8 @@ import { markdownSchema } from 'sanity-plugin-markdown';
 
 export default createConfig({
   name: 'default',
-  title: 'leerob.io',
-  projectId: '56a8ffnw',
+  title: 'portfolio',
+  projectId: '1sigoea0',
   dataset: 'production',
   plugins: [deskTool(), markdownSchema()],
   schema: {
